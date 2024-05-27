93983694317c0a5e3d156ec56baa44d84114e318
Tomas Valenta <valenta.and.thomas@gmail.com>
2024-05-27 14:17:59 +0200

    Improve start script and python script

--- python/example.py
+++ python/example.py
@@ -1,43 +1,70 @@
 from e2b import Sandbox
 from dotenv import load_dotenv
+import uuid
+
 
 load_dotenv()
 
 
-code = """
+class Desktop(Sandbox):
+    DISPLAY = ":99"  # Must be the same as the start script env var
+
+    def __init__(self):
+        super().__init__(template="desktop")
+
+    def screenshot(self, name: str):
+        print("Getting screenshot")
+        screenshot_path = "/home/user/screenshot.png"
+        result = self.process.start_and_wait(
+            f"import -window root {screenshot_path}",
+            env_vars={"DISPLAY": self.DISPLAY},
+        )
+        print(result.stderr)
+
+        print("Downloading screenshot")
+        file = self.download_file(screenshot_path)
+        with open(name, "wb") as f:
+            f.write(file)
+
+    @staticmethod
+    def _wrap_pyautogui_code(code: str):
+        return f"""
 import pyautogui
 import os
-from pyvirtualdisplay.display import Display
 import Xlib.display
-
-disp = Display(visible=True, size=(1024, 768), backend="xvfb", use_xauth=True)
-disp.start()
 
 pyautogui._pyautogui_x11._display = Xlib.display.Display(os.environ["DISPLAY"])
 
-pyautogui.write('Hello world!')
-pyautogui.move(400, 0)
+{code}
 
-disp.stop()
+exit(0)
 """
 
-with Sandbox(template="desktop") as sandbox:
-    # Manipulate desktop
-    sandbox.filesystem.write("/home/user/code.py", code)
-    result = sandbox.process.start_and_wait(
-        "python /home/user/code.py",
-        env_vars={"DISPLAY": ":1"},
-    )
-    print(result.stderr)
+    def control(self, pyautogui_code: str):
+        code_path = f"/home/user/code-{uuid.uuid4()}.py"
 
-    # Get screenshot
-    screenshot_path = "/home/user/screenshot.png"
-    result = sandbox.process.start_and_wait(
-        f"scrot && import -window root {screenshot_path}",
-        env_vars={"DISPLAY": ":1"},
-    )
-    print(result.stdout)
+        code = self._wrap_pyautogui_code(pyautogui_code)
 
-    file = sandbox.download_file(screenshot_path)
-    with open("screenshot.png", "wb") as f:
-        f.write(file)
+        print("Writing code")
+        self.filesystem.write(code_path, code)
+
+        print("Running code")
+        self.process.start_and_wait(
+            f"python {code_path}",
+            on_stdout=lambda stdout: print(stdout),
+            on_stderr=lambda stderr: print(stderr),
+            env_vars={"DISPLAY": self.DISPLAY},
+        )
+
+
+with Desktop() as desktop:
+    desktop.control(
+        """
+currentMouseX, currentMouseY = pyautogui.position() # Get the XY position of the mouse.
+print(currentMouseX, currentMouseY)
+
+pyautogui.moveTo(100, 150) # Move the mouse to XY coordinates.
+    """
+    )
+
+    desktop.screenshot("screenshot.png")
--- template/start-up.sh
+++ template/start-up.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-export DISPLAY=:1
+export DISPLAY=:99
 
-Xvfb :1 -ac -screen 0 1024x768x16 &
-sleep 1
-DISPLAY=:1 /usr/bin/xfce4-session
+Xvfb $DISPLAY -ac -screen 0 1024x768x16 &
+/usr/bin/xfce4-session
