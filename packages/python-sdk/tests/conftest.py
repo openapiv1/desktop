ba3bd0dec88f73271025872a8e0699ac63b0304e
Vasek Mlejnsky <vasek.mlejnsky@gmail.com>
2024-11-23 21:44:29 -0800

    Move python SDK to 'packages'

---
+++ packages/python-sdk/tests/conftest.py
@@ -0,0 +1,34 @@
+import pytest
+import os
+
+from logging import warning
+
+from e2b_desktop import Sandbox
+
+timeout = 60
+
+@pytest.fixture()
+def sandbox(debug):
+    sandbox = Sandbox(timeout=timeout)
+
+    try:
+        yield sandbox
+    finally:
+        try:
+            sandbox.kill()
+        except:
+            if not debug:
+                warning(
+                    "Failed to kill sandbox — this is expected if the test runs with local envd."
+                )
+
+
+@pytest.fixture
+def debug():
+    return os.getenv("E2B_DEBUG") is not None
+
+@pytest.fixture(autouse=True)
+def skip_by_debug(request, debug):
+    if request.node.get_closest_marker("skip_debug"):
+        if debug:
+            pytest.skip("skipped because E2B_DEBUG is set")
